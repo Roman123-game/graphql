1749972defca4f7f68632b2a7f2797349371b5a8
RB <82284311+Roman123-game@users.noreply.github.com>
2022-06-03 18:15:11 +0300

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,67 +1,69 @@
-import { useQuery, useMutation } from '@apollo/client';
-import { useEffect, useState } from 'react';
-import './App.css';
-import { CREATE_USER } from './mutation/user.js';
-import { GET_ALL_USERS, GET_ONE_USER } from "./query/user.js"
+import React, {useEffect, useState} from 'react';
+import './App.css'
+import {useMutation, useQuery} from "@apollo/client";
+import {GET_ALL_USERS, GET_ONE_USER} from "./query/user";
+import {CREATE_USER} from "./mutation/user";
 
-function App() {
-  const { data, loading, error, refetch } = useQuery(GET_ALL_USERS)
-  const { data: oneUser, loading: loadingOneUser, } = useQuery(GET_ONE_USER, {
-    variables: {
-      id: 1
-
-    }
-  })
-  const [users, setUsers] = useState([]);
-  const [username, setUsername] = useState('');
-  const [age, setAge] = useState(0);
-  const [newUser] = useMutation(CREATE_USER);
-  console.log(data)
-  useEffect(() => {
-    if (!loading) {
-      setUsers(data.getAllUsers)
-    }
-    if (loading) {
-      <h1>loading ....</h1>
-    }
-  }, [data]);
-
-  const addUser = (e) => {
-    e.preventDefault();
-    newUser({
-      variables: {
-        input: {
-          username, age
+const App = () => {
+    const {data, loading, error, refetch} = useQuery(GET_ALL_USERS)
+    const {data:oneUser, loading: loadingOneUser} = useQuery(GET_ONE_USER, {
+        variables: {
+            id: 1
         }
-      }
-    }).then(({ data }) => {
-      console.log(data);
-      setUsername(" ");
-      setAge(0);
     })
-  }
-  const getAll = e => {
-    e.preventDefault();
-    refetch();
-  }
-  return (
-    <div className="App">
-      <form>
-        <input value={username} onChange={e => setUsername(e.target.value)} type="text"></input>
-        <input value={age} onChange={e => setAge(e.target.value)} type="number"></input>
-        <div >
-          <button onClick={(e) => addUser(e)} className='button'> create</button>
-          <button onClick={(e) => getAll(e)} className='button'> Recieve</button>
-        </div>
-      </form>
-      <div>
-        {users.map(user =>
-          <div className='user' key={user.id}>{user.id}.{user.username}.{user.age}</div>
-        )}
-      </div>
-    </div>
+    const [newUser] = useMutation(CREATE_USER)
+    const [users, setUsers] = useState([])
+    const [username, setUsername] = useState('')
+    const [age, setAge] = useState(0)
 
-  );
-}
+    console.log(oneUser)
+
+    useEffect(() => {
+        if (!loading) {
+            setUsers(data.getAllUsers)
+        }
+    }, [data])
+
+    const addUser = (e) => {
+        e.preventDefault()
+        newUser({
+            variables: {
+                input: {
+                    username, age
+                }
+            }
+        }).then(({data}) => {
+            console.log(data)
+            setUsername('')
+            setAge(0)
+        })
+    }
+    const getAll = e => {
+        e.preventDefault()
+        refetch()
+    }
+
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+
+    return (
+        <div>
+            <form>
+                <input value={username} onChange={e => setUsername(e.target.value)} type="text"/>
+                <input value={age} onChange={e => setAge(e.target.value)} type="number"/>
+                <div className="btns">
+                    <button className="butons"onClick={(e) => addUser(e)}>Создать</button>
+                    <button className="butons"onClick={e => getAll(e)}>Получить</button>
+                </div>
+            </form>
+            <div>
+                {users.map(user =>
+                    <div key={user.id} className="user">{user.id}. {user.username} {user.age}</div>
+                )}
+            </div>
+        </div>
+    );
+};
 
 export default App;
