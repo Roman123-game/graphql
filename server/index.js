484c1f84e966cb8dbd11c9b32ee5f023df8a6bc3
RB <82284311+Roman123-game@users.noreply.github.com>
2022-06-03 13:55:06 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const {graphqlHTTP} =("express-graphql")
+const {graphqlHTTP} =("express-graphql");
+const cors = require('cors');
 const app = express();
 const PORT = 5000;
 app.listen(PORT,()=>console.log(`server runnning on `,{PORT}));
