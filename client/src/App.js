627cfb68d671898fa87281944b9528323d67081b
RB <82284311+Roman123-game@users.noreply.github.com>
2022-06-03 18:01:24 +0300

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,58 @@
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { useEffect, useState } from 'react';
 import './App.css';
-import {GET_ALL_USERS} from "./query/user.js"
+import { CREATE_USER } from './mutation/user.js';
+import { GET_ALL_USERS } from "./query/user.js"
 
 function App() {
-   const {data,loading,error} = useQuery(GET_ALL_USERS)
+  const { data, loading, error,refetch } = useQuery(GET_ALL_USERS)
   const [users, setUsers] = useState([]);
-console.log(data)
+  const [username, setUsername] = useState('');
+  const [age, setAge] = useState(0);
+  const [newUser] = useMutation(CREATE_USER);
+  console.log(data)
   useEffect(() => {
-    if(!loading){
+    if (!loading) {
       setUsers(data.getAllUsers)
     }
-    if(loading){
-      return <h1>loading ....</h1>
+    if (loading) {
+      <h1>loading ....</h1>
     }
-  },[data]);
+  }, [data]);
 
-
-
+  const addUser = (e) => {
+    e.preventDefault();
+    newUser({
+      variables: {
+        input: {
+          username, age
+        }
+      }
+    }).then(({ data }) => {
+      console.log(data);
+      setUsername(" ");
+      setAge(0);
+    })
+  }
+const getAll = e =>{
+  e.preventDefault();
+  refetch();
+}
   return (
     <div className="App">
- <form>
-   <input type="text"></input>
-   <input type="number"></input>
-   <div >
-     <button className='button'> create</button>
-     <button className='button'> Recieve</button>
-   </div>
- </form>
- <div>
- {users.map(user=>
-<div className='user' key={user.id}>{user.id}.{user.username}.{user.age}</div>
- )}
- </div>
+      <form>
+        <input value={username} onChange={e => setUsername(e.target.value)} type="text"></input>
+        <input value={age} onChange={e => setAge(e.target.value)} type="number"></input>
+        <div >
+          <button onClick={(e)=>addUser(e)} className='button'> create</button>
+          <button onClick={(e)=>getAll(e)} className='button'> Recieve</button>
+        </div>
+      </form>
+      <div>
+        {users.map(user =>
+          <div className='user' key={user.id}>{user.id}.{user.username}.{user.age}</div>
+        )}
+      </div>
     </div>
 
   );
